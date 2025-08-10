0981f28e8a49b0b704c4e17b6bd158dfd8603c8a
Gihan Tharuka <141792890+gihan-tharuka@users.noreply.github.com>
2025-08-10 07:19:47 +0530

    login

--- src/App.js
+++ src/App.js
@@ -6,31 +6,41 @@ import Patients from './pages/Patients';
 import Monitoring from './pages/Monitoring';
 import Alerts from './pages/Alerts';
 import Analytics from './pages/Analytics';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import { withAuthenticator } from '@aws-amplify/ui-react';
+// Amplify imports
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 
+// Import the new Authenticator component & default styles
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+// Configure Amplify
 Amplify.configure(awsconfig);
 
-function App({ signOut, user }) {
+function App() {
   return (
-    <div className="App">
-      <Router>
-        <Layout signOut={signOut} user={user}>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/patients" element={<Patients />} />
-            <Route path="/monitoring" element={<Monitoring />} />
-            <Route path="/alerts" element={<Alerts />} />
-            <Route path="/analytics" element={<Analytics />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <Router>
+            <Layout signOut={signOut} user={user}>
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/patients" element={<Patients />} />
+                <Route path="/monitoring" element={<Monitoring />} />
+                <Route path="/alerts" element={<Alerts />} />
+                <Route path="/analytics" element={<Analytics />} />
+              </Routes>
+            </Layout>
+          </Router>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
